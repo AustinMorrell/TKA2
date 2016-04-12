bbe943f4782ef13b91c1cbc68b0f7c8cfa05d17b
Austin Morrell <austinlm19@gmail.com>
2016-04-12 09:44:37 -0500

    Finished without music

--- Techno Kitten Adventure 2/Assets/_Scripts/GameCon.cs
+++ Techno Kitten Adventure 2/Assets/_Scripts/GameCon.cs
@@ -5,11 +5,9 @@ public class GameCon : MonoBehaviour {
 
     [SerializeField]
     public Player Char;
-    private SaveLoad Word = new SaveLoad();
 
     void Start()
     {
-        Word.Load();
         Instantiate(Char, new Vector3(0, -4, 0), Quaternion.identity);
     }
 }
--- Techno Kitten Adventure 2/Assets/_Scripts/Player.cs
+++ Techno Kitten Adventure 2/Assets/_Scripts/Player.cs
@@ -7,19 +7,32 @@ public class Player : MonoBehaviour {
 
     public float speed;
 
+    [Serializable]
+    protected class Box
+    {
+        public float MinX, MinY, MaxX, MaxY;
+    }
+
+    [SerializeField]
+    protected Box m_ScreenBorders;
+
     // Use this for initialization
     void Start () {
-	
+        m_ScreenBorders.MaxX = 9;
+        m_ScreenBorders.MaxY = 5;
+        m_ScreenBorders.MinX = -9;
+        m_ScreenBorders.MinY = -5;
 	}
 
     void Lerp(float a, float b, float c)
     {
-        gameObject.transform.position = new Vector3(transform.position.x + a, transform.position.y + b, transform.position.z + c);
+        gameObject.transform.position = new Vector3(transform.position.x + a, transform.position.y - b, transform.position.z + c);
     }
 
     // Update is called once per frame
     void Update ()
     {
         Lerp(Input.GetAxis("Horizontal") * speed, -Input.GetAxis("Vertical") * speed, 0);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, m_ScreenBorders.MinX, m_ScreenBorders.MaxX), Mathf.Clamp(transform.position.y, m_ScreenBorders.MinY, m_ScreenBorders.MaxY), transform.position.z);
     }
 }
